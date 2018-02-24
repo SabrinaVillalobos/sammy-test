733aa1ab2b651043c24684e5bbfdb58a6ff04420
SabrinaVillalobos <wafflish@gmail.com>
2018-02-24 18:54:52 -0300

    test

---
+++ javascripts/kiphu.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var khipu = require ('khipu-api-client');
+    KhipuLib.onLoad({
+        elementId: 'pay-button'
+        , data: {
+                "id":"vzkeh1wk82ax"
+                ,"bill-id":"V1A6y"
+                ,"url":"https://khipu.com/payment/show/vzkeh1wk72ax"
+                ,"manual-url":"https://khipu.com/payment/manual/vzkeh1wk72ax"
+                ,"mobile-url":"khipu:///pos/vzkeh1wk72ax"
+                ,"ready-for-terminal":true}
+    })
+}
